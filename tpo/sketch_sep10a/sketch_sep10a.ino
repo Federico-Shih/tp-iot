1a01d55afc3c9f25fac838eb830c66979c9c25b1
sh1ft <fedeshih@gmail.com>
2024-09-10 21:36:06 -0300

    feat: initial arduino with MQTT

---
+++ tpo/sketch_sep10a/sketch_sep10a.ino
@@ -0,0 +1,73 @@
+#include <RIC3D.h>
+#include <RIC3DMODEM.h>
+
+RIC3D device();
+
+#define SerialMon Serial
+
+#define SerialAT Serial3
+
+// Module baud rate
+uint32_t rate = 115200; 
+
+// Select SIM Card (0 = right, 1 = left)
+bool sim_selected = 1; 
+
+const char apn[]      = "grupotesacom.claro.com.ar";
+const char gprsUser[] = "";
+const char gprsPass[] = "";
+
+const char mqtt_ip[] = "10.25.1.152";
+const char mqtt_port[] = "4099";
+
+const char user_name[] = "BbLqC3W041ZSlg3arnFN"; // aca se debe poner el id del device de tdata
+
+int sensorValue = 0;
+float temperature = 0;
+float voltage = 0;
+char str[7];
+
+int interval = 10000;
+long old_millis = 0;
+
+void setup() 
+{
+  SerialMon.begin(115200);
+  SerialMon.println(F("***********************************************************"));
+  SerialMon.println(F(" Initializing Modem"));
+  pinMode(SIM_SELECT,OUTPUT);
+  digitalWrite(SIM_SELECT,sim_selected);
+  SerialMon.print(" Sim selected is the one on the ");
+  SerialMon.println(sim_selected?"left":"right");
+  ModemBegin(&SerialAT,&SerialMon);
+  ModemTurnOff();
+  ModemTurnOn();
+  SerialAT.begin(rate);
+  analogReference(INTERNAL2V56);
+  SerialMon.println(" Opening MQTT service ");
+  if(CreatePDPContext(apn, gprsUser,  gprsPass))
+  {
+    SerialMon.println("Error creating PDP context");
+  }
+  ActivatePDPContext();
+  if(ConnectMQTTClient(user_name,mqtt_ip,mqtt_port))
+  {
+    SerialMon.println("Error connecting to tdata");
+  }
+}
+
+void loop() 
+{
+  if((millis() - old_millis) > interval)
+  {
+    old_millis = millis();
+    sensorValue = analogRead(TEMPSENSOR);
+    temperature = (float(sensorValue)*25/100)-60;
+    sensorValue = analogRead(VCCSENSE);
+    voltage = (float(sensorValue)*(25*11)/10000);
+    dtostrf(temperature, 4, 2, str);
+    PublishData("Temperatura",str);
+    dtostrf(voltage, 4, 2, str);
+    PublishData("Tension",str);
+  }
+}
